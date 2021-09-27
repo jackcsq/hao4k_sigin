e4ea5f287b72c4eb6b5b67af5c8d6d9b2eafec78
zhijun.liu <12345678>
2021-09-27 17:01:15 +0800

    init

---
+++ hao4k.py
@@ -0,0 +1,43 @@
+#/* 
+# 好4k 签到领金币
+#[task_local]
+# 好4k 签到领金币
+# 0 0 09 * *
+#*/
+import requests
+import os
+import re
+username = "ghostwolf1314"
+password = "4161290liu"
+
+# hao4k 签到 url
+user_url = "https://www.hao4k.cn//member.php?mod=logging&action=login"
+base_url = "https://www.hao4k.cn/"
+signin_url = "https://www.hao4k.cn/plugin.php?id=k_misign:sign&operation=qiandao&formhash={formhash}&format=empty"
+form_data = {
+    'formhash': "",
+    'referer': "https://www.hao4k.cn/",
+    'username': username,
+    'password': password,
+    'questionid': "0",
+    'answer': ""
+}
+inajax = '&inajax=1'
+
+def run(form_data):
+    s = requests.Session()
+    s.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36'})
+    headers = {"Content-Type": "text/html", 'Connection': 'close'}
+    user_resp = s.get(user_url,headers=headers)
+    login_text = re.findall('action="(.*?)"', user_resp.text)
+    for loginhash in login_text:
+        if 'loginhash' in loginhash:
+            login_url = base_url + loginhash + inajax
+            login_url = login_url.replace("amp;", "")
+            print(login_url)
+    form_text = re.search('formhash=(.*?)\'', user_resp.text)
+    print(form_text.group(1))
+    form_data['formhash'] = form_text.group(1)
+    print(form_data)
+
+run(form_data)
